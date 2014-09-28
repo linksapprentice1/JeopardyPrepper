5e6bfe5b91dd647a2df1f5fc7504c22b067559ce
linksapprentice1 <linksapprentice1@gmail.com>
2014-09-28 16:49:02 -0700

    Recleaned formatting of dictionary :P

--- JeopardyCategories.py
+++ JeopardyCategories.py
@@ -1,10 +1,7 @@
 def categoriesByRound():
    return {
       "All Rounds" : ["Before & After", "Literature", "Science", "Word Origins", "American History", "State Capitals", "World History", "Business and Industry",  "Potpourri", "World Geography"], 
-
       "Jeopardy" :  ["Sports", "Potpourri", "Animals", "Stupid Answers", "American History", "Science", "State Capitals", "Television", "U.S. Cities", "Pop Music", "Transportation"],
-
       "Double Jeopardy" : ["Opera", "Ballet", "Art", "Shakespeare", "Islands"],
-
       "Final Jeopardy" :  ["U.S. Presidents", "Authors", "World Leaders", "Historic Names", "Famous Americans", "Famous Names", "Americana", "Organizations"]
    }
