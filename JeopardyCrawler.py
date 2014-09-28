048b59c8ad8b99cc5db9fc48d3dc69005598c602
linksapprentice1 <linksapprentice1@gmail.com>
2014-09-28 16:18:06 -0700

    Additional clean-up

--- JeopardyCrawler.py
+++ JeopardyCrawler.py
@@ -8,18 +8,16 @@ collected_data = []
 
 def fetchQuestions(categories_by_round):
    http = urllib3.PoolManager()
-   threads = []
-   for the_round, categories in categories_by_round.iteritems(): 
-      for category in categories:
-         threads.append(threader.makeThread(_fetchQuestionsPerCategory, (http, the_round, category)))
-   threader.joinThreads(threads)
+   threader.joinThreads(_threadPerCategory(http, categories_by_round))
    return collected_data
 
+def _threadPerCategory(http, categories_by_round):
+   for the_round, categories in categories_by_round.iteritems(): 
+      for category in categories:
+         yield threader.makeThread(_fetchQuestionsPerCategory, (http, the_round, category))
+
 def _fetchQuestionsPerCategory(http, the_round, category):
-   url = "http://j-archive.com/search.php?search=" + urllib.quote_plus(category) + "&submit=Search"
-   webpage = http.request('GET', url).data
-   soup = BeautifulSoup.BeautifulSoup(webpage)
-   text = soup.findAll(text=True)
+   text = _text(_webpage(http, _url(category)))
    for question_and_answer in _questionAndAnswers(text):
       if all(question_and_answer):
          collected_data.append({   
@@ -28,6 +26,15 @@ def _fetchQuestionsPerCategory(http, the_round, category):
             "question" : question_and_answer[0],
             "answer" : question_and_answer[1]
          })
+
+def _text(webpage):
+   return BeautifulSoup.BeautifulSoup(webpage).findAll(text=True)
+
+def _webpage(http, url):
+   return http.request('GET', url).data
+
+def _url(category):
+   return "http://j-archive.com/search.php?search=" + urllib.quote_plus(category) + "&submit=Search"
 
 def _questionAndAnswers(elements):
    for i in _clueValueIndices(elements):
@@ -46,4 +53,3 @@ def _nextText(elements, i):
 
 def _cleanUp(element):
    return element.strip().replace(": (", "").replace(":","").encode('utf8', 'replace')
-
