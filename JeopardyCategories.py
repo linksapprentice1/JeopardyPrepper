2ef211de26c24bd37231478042570048b448dc4b
linksapprentice1 <linksapprentice1@gmail.com>
2014-09-28 16:47:31 -0700

    Cleaned up formatting of dictionary

--- JeopardyCategories.py
+++ JeopardyCategories.py
@@ -1,36 +1,10 @@
 def categoriesByRound():
-   All_Rounds = ["Before & After",
-                 "Literature",
-                 "Science",
-                 "Word Origins",
-                 "American History",
-                 "State Capitals",
-                 "World History",
-                 "Business and Industry",
-                 "Potpourri",
-                 "World Geography"]
-   Jeopardy =   ["Sports",
-                 "Potpourri",
-                 "Animals",
-                 "Stupid Answers",
-                 "American History",
-                 "Science",
-                 "State Capitals",
-                 "Television",
-                 "U.S. Cities",
-                 "Pop Music",
-                 "Transportation"]
-   Double_Jeopardy = ["Opera",
-                      "Ballet",
-                      "Art",
-                      "Shakespeare",
-                      "Islands"]
-   Final_Jeopardy =  ["U.S. Presidents",
-                      "Authors",
-                      "World Leaders",
-                      "Historic Names",
-                      "Famous Americans",
-                      "Famous Names",   
-                      "Americana",
-                      "Organizations"]
-   return {"All Rounds" : All_Rounds, "Jeopardy" : Jeopardy, "Double Jeopardy" : Double_Jeopardy, "Final Jeopardy" : Final_Jeopardy}
+   return {
+      "All Rounds" : ["Before & After", "Literature", "Science", "Word Origins", "American History", "State Capitals", "World History", "Business and Industry",  "Potpourri", "World Geography"], 
+
+      "Jeopardy" :  ["Sports", "Potpourri", "Animals", "Stupid Answers", "American History", "Science", "State Capitals", "Television", "U.S. Cities", "Pop Music", "Transportation"],
+
+      "Double Jeopardy" : ["Opera", "Ballet", "Art", "Shakespeare", "Islands"],
+
+      "Final Jeopardy" :  ["U.S. Presidents", "Authors", "World Leaders", "Historic Names", "Famous Americans", "Famous Names", "Americana", "Organizations"]
+   }
